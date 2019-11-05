4b1bcebc14114c2c05b1747314a103dfd4923977
Michael W <56413040+michaelGRU@users.noreply.github.com>
2019-11-04 22:30:44 -0800

    Add files via upload

---
+++ gru.rb
@@ -0,0 +1,18 @@
+#@michaelGRU
+#@Topological Quantum Matter
+
+#parameters and logic
+#define para
+img = "simple matter"
+img_one = "matter 1"
+img_two = "matter 2"
+#define quan
+vp = 20
+#set boolean
+istrue = true
+isfalse = false
+############################
+#no value
+isnil = nil
+############################
+puts vp ** 2
